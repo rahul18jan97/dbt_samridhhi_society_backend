2b629101839f4fa09625713c6bc8c2997ec6dbfc
rahul18jan97 <rahul18jan97@gmail.com>
2026-02-16 23:17:27 +0530

    Remove firebase service account and add gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,22 +1,22 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../config/firebase-service-account");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
 // const admin = require("firebase-admin");
-
-// const serviceAccount = JSON.parse(
-//   Buffer.from(
-//     process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
-//     "base64"
-//   ).toString("utf8")
-// );
+// const serviceAccount = require("../config/firebase-service-account");
 
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 // });
 
 // module.exports = admin;
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(
+  Buffer.from(
+    process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
+    "base64"
+  ).toString("utf8")
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+module.exports = admin;
